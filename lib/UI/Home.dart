c658f0a2f94c31792c816723918ea4b2af72b2e4
srbshameer098 <srbshameer098@gmail.com>
2023-12-18 17:39:59 +0530

    music

--- lib/UI/Home.dart
+++ lib/UI/Home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:untitled4/UI/Music.dart';
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -26,50 +27,56 @@ class _HomeState extends State<Home> {
             Text(' Played',style: TextStyle(color: Colors.white,fontSize: 24.sp,fontWeight: FontWeight.w600),),
                   SizedBox(height: 10,),
             
-                  SizedBox(height: 250,
-                    child: GridView.count(
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 5,
-                      mainAxisSpacing: 5,
-                      childAspectRatio: 150 / 50,
-                      shrinkWrap: true,
-                      children: List.generate(
-                      8,
-                            (index) {
-                          return Container(
-                            width: 150.w,
-                            height: 70.h,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(10.0),
-                              ),color: Colors.grey.shade700
-                            ),
-                            child:Row(
-                              children: [
-                                Container( width: 50.w,height: 80.h,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.only(
-                                          bottomRight: Radius.circular(0),
-                                          bottomLeft: Radius.circular(10),
-                                          topRight: Radius.circular(0),
-                                          topLeft: Radius.circular(10)
-            
-            
-                                      ),color: Colors.blueAccent
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (_)=>Music()));
+                    },
+                    child: SizedBox(height: 250,
+                      child: GridView.count(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 5,
+                        mainAxisSpacing: 5,
+                        childAspectRatio: 150 / 50,
+                        shrinkWrap: true,
+                        children: List.generate(
+                        8,
+                              (index) {
+                            return Container(
+                              width: 150.w,
+                              height: 70.h,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10.0),
+                                ),color: Colors.grey.shade700
+                              ),
+                              child:Row(
+                                children: [
+                                  Container( width: 50.w,height: 80.h,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.only(
+                                            bottomRight: Radius.circular(0),
+                                            bottomLeft: Radius.circular(10),
+                                            topRight: Radius.circular(0),
+                                            topLeft: Radius.circular(10)
+
+
+                                        ),color: Colors.blueAccent
+                                    ),
+                                    child: Image.asset('assets/img1.jpg',fit: BoxFit.fill,
+
+                                    ),
                                   ),
-                                  child: Image.asset('assets/img1.jpg',fit: BoxFit.fill,
-            
+
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text('data',style: TextStyle(color: Colors.white),),
                                   ),
-                                ),
-            
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text('data',style: TextStyle(color: Colors.white),),
-                                ),
-                              ],
-                            )
-                          );
-                        },
+                                ],
+                              )
+                            );
+                          },
+                        ),
                       ),
                     ),
                   ),
--- lib/UI/Music.dart
+++ lib/UI/Music.dart
@@ -8,6 +8,7 @@ class Music extends StatefulWidget {
 }
 
 class _MusicState extends State<Music> {
+  double Value = 20;
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -20,28 +21,160 @@ class _MusicState extends State<Music> {
         ),
 
 
-        title: Column(
-          children: [
-            Text(
-              'Playing From Playlist '
-            ),
+        title: Center(
+          child: Column(
+            children: [
+              Text(
+                'Playing From Playlist ',style: TextStyle(color: Colors.white,fontSize: 12.sp),
+              ),
 
 
-            Text(
-                'New Songs '
-            ),
+              Text(
+                  'New Songs ',style: TextStyle(color: Colors.white,fontSize: 14.sp,fontWeight: FontWeight.w500)
+              ),
 
 
-          ],
+            ],
+          ),
         ),
+
+        actions: [
+
+          Icon(Icons.more_horiz,color: Colors.white,),
+          SizedBox(width: 20.w,),
+        ],
 
       ),
 
-      body: Column(
-        children: [
+      body: SafeArea(
+        child: Padding(
+          padding:  EdgeInsets.only(left: 0.w,top: 10.h),
+          child: Column(
+            children: [
 
-        ],
+
+              // -----------------------------  Container 1  ---------------------------------
+
+
+              Center(
+                child: Container(
+                  width: 300.w,
+                  height: 400.h,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),color: Colors.grey.shade700
+                  ),
+                ),
+              ),
+
+
+              Padding(
+                padding:  EdgeInsets.only(left:28.w,top: 40.sp),
+                child: Row(
+                  children: [
+
+                    Column(
+                      children: [
+                        Text(
+                          'Playing From Playlist ',style: TextStyle(color: Colors.white,fontSize: 20.sp,fontWeight: FontWeight.w700),
+                        ),
+
+
+                        Text(
+                            'Playing From Playlist New Songs ',style: TextStyle(color: Colors.white,fontSize: 12.sp,fontWeight: FontWeight.w500)
+                        ),
+                      ],
+                    ),
+
+                    
+                    SizedBox(width: 100.w,),
+                    Icon(Icons.favorite_border,color: Colors.grey,)
+                    
+                  ],
+                ),
+              ),
+SizedBox(height: 10.h,),
+
+              Slider(
+                min: 0.0,
+                max: 100.0,
+                activeColor: Colors.white,
+                inactiveColor: Colors.grey.shade900,
+                thumbColor: Colors.white,
+                value: Value,
+                onChanged: (value) {
+                  setState(() {
+                    Value = value;
+                  });
+                },
+              ),
+
+              Padding(
+                padding:  EdgeInsets.only(left: 22.w,top: 1.h),
+                child: Row(
+                  children: [
+
+                    Text(
+                      '00:00',style: TextStyle(color: Colors.white,fontSize: 12.sp,fontWeight: FontWeight.w500),
+                    ),
+SizedBox(width: 260.w,),
+
+                    Text(
+                        '00:00',style: TextStyle(color: Colors.white,fontSize: 12.sp,fontWeight: FontWeight.w500)
+                    ),
+
+                  ],
+                ),
+              ),
+
+              Padding(
+                padding:  EdgeInsets.only(left: 22.w,top: 10.h),
+                child: Row(
+                  children: [
+
+                    Icon(Icons.shuffle_outlined,color: Colors.white,size: 30,),
+                    SizedBox(width: 35.w,),
+
+                    Icon(Icons.skip_previous,color: Colors.white,size: 35,),
+                    SizedBox(width: 20.w,),
+
+
+                    CircleAvatar(
+                        radius: 35,backgroundColor: Colors.white,
+                        child: Icon(Icons.pause,color: Colors.black,size: 35,)),
+
+
+                    SizedBox(width: 20.w,),
+
+                    Icon(Icons.skip_next,color: Colors.white,size: 35,),
+
+                    SizedBox(width: 35.w,),
+
+                    Icon(Icons.repeat_outlined,color: Colors.white,size: 35,)
+                  ],
+                ),
+              ),
+
+              Row(
+                children: [
+
+                  SizedBox(width: 22.w,),
+
+                  Icon(Icons.devices,color: Colors.white,size: 22,),
+
+                  SizedBox(width: 280.w,),
+
+                  Icon(Icons.share_outlined,color: Colors.white,size: 22,)
+
+                ],
+              )
+
+            ],
+          ),
+        ),
       ),
     );
   }
+
 }
